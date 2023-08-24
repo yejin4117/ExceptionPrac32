4ad6ae178e7b19f6070fa5c91153547466bc7694
yejin4117 <s2108@e-mirim.hs.kr>
2023-08-24 10:05:24 +0900

    직접 예외 정의 및 발생시키기

--- ExceptionPrac32/Program.cs
+++ ExceptionPrac32/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace ExceptionPrac32
 {
-    internal static class Program
+    internal class Program
     {
+        class WrongNumberException : Exception
+        {
+            public WrongNumberException(string message) : base(message)
+            {
+                // 예외가 발생했을 때 특정한 작업을 할 코드를 여기에 적을 수 있다.
+            }
+        }
+
         static void Main(string[] args)
         {
             string[] array = { "가", "나" };
@@ -16,6 +25,12 @@ namespace ExceptionPrac32
             try
             {
                 input = int.Parse(Console.ReadLine());
+
+                if (input < 0 || input > 999)
+                {
+                    throw new WrongNumberException("0이상 999이하여야 합니다");
+                }
+
                 Console.WriteLine("입력한 위치의 값은 '" + array[input]
     + "' 입니다.");
             }
@@ -26,6 +41,11 @@ namespace ExceptionPrac32
             catch (IndexOutOfRangeException ex)
             {
                 Console.WriteLine("인덱스 범위를 벗어났습니다");
+            }
+            catch (WrongNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("제대로 값을 입력하란 말이야!");
             }
             catch (Exception ex)
             {
