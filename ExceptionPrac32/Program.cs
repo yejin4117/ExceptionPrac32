f1f1ca965a51fdaac3f9bf710e7e02fb3fd30255
yejin4117 <s2108@e-mirim.hs.kr>
2023-08-24 09:49:21 +0900

    tray catch를 이용한 다양한 예외 처리

--- ExceptionPrac32/Program.cs
+++ ExceptionPrac32/Program.cs
@@ -12,17 +12,27 @@ namespace ExceptionPrac32
         {
             string[] array = { "가", "나" };
             Console.WriteLine("숫자를 입력해 주세요:");
-            int input = int.Parse(Console.ReadLine());
-
-            if (input < array.Length)
+            int input = 0;
+            try
             {
+                input = int.Parse(Console.ReadLine());
                 Console.WriteLine("입력한 위치의 값은 '" + array[input]
-                + "' 입니다.");
+    + "' 입니다.");
             }
-            else
+            catch (FormatException ex)
             {
-                Console.WriteLine("인덱스 범위를 넘었습니다!");
+                Console.WriteLine("숫자만 입력하세요!!");
             }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("인덱스 범위를 벗어났습니다");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
